ae10f81501dd117bc99f08a5848a530097cbf3ae
unknown <yli@MSC-R96RKT6.allegisgroup.com>
2014-02-12 17:44:39 -0500

    add support for mix up

--- applications/mix-up/js/index.js
+++ applications/mix-up/js/index.js
@@ -1,6 +1,27 @@
 define(function(require,exports,module){
 	module.exports.onEnter=function(){
 		$.mobile.activePage.css("background","#3498DB");
+		var pumperConfig=require("js/services/pumperConfig").pumperConfig;
+
+		pumperConfig.on('list',function(){
+			var listview=new comp.Listview("#pumper_list");
+			listview.render(this.list,function(item,$li){
+				var $title=$('<h1>Pumper#{code}-{name}</h1>'.bind(item));
+				
+				var $div=$("<div>");
+				$div.append('<input type="range" name="slider1" id="slider1" value="50" min="0" max="100" data-highlight="true">');
+				$div.find("input").slider();
+
+				//I have to fix the bug of slider
+
+				$div.find("input").attr({
+					type:"number",
+					"class":"ui-input-text ui-body-c ui-corner-all ui-shadow-inset ui-slider-input"
+				});
+
+			 	$li.append($title).append($div);
+			 })
+		}).list();
 	}
 	module.exports.try=function(){
 		var maker=require("js/services/cocktailsMaker");
--- js/services/pumperConfig.js
+++ js/services/pumperConfig.js
@@ -4,18 +4,15 @@ define(function(require,exports,module){
 
   PumperConfig.prototype.list=function(){
     var that=this;
-    this.list=[
-      {code:0,drink_code:1,name:"sample drink 1"},
-      {code:1,drink_code:2,name:"sample drink 2"},
-      {code:2,drink_code:3,name:"sample drink 3"},
-      {code:3,drink_code:4,name:"sample drink 4"},
-      {code:4,drink_code:5,name:"sample drink 5"},
-      {code:5,drink_code:6,name:"sample drink 6"},
-      {code:6,drink_code:7,name:"sample drink 7"},
-      {code:7,drink_code:8,name:"sample drink 8"}
+    var sql="select pumperConfig.code as code,drink_code,name \
+    from pumperConfig join drink where pumperConfig.drink_code=drink.code"
 
-    ]
-    that.trigger("list");
+    var dbs=new entity.Base();
+    dbs.on('listBySql',function(){
+      that.list=this.entities;
+      that.trigger("list");
+    }).listBySql(sql);
+
   }
 
   module.exports.pumperConfig=new PumperConfig();
